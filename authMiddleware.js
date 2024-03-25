8352f4a6e210d774ecaf174e1218a6c9ef9dac7d
Fleshren <fleshren1@gmail.com>
2024-03-25 02:24:33 -0500

    Refactor authMiddleware.js to use object literal syntax

--- authMiddleware.js
+++ authMiddleware.js
@@ -1,31 +1,34 @@
-exports.isAuthenticated = (req, res, next) => {
-  try {
-    if (req.session.user) {
-      // User is authenticated
-      next();
-    } else {
-      // User is not authenticated
-      res.redirect('/signin');
+const authMiddleware = {
+  isAuthenticated: (req, res, next) => {
+    try {
+      if (req.session.user) {
+        // User is authenticated
+        next();
+      } else {
+        // User is not authenticated
+        res.redirect('/signin');
+      }
+    } catch (error) {
+      console.error('Error in isAuthenticated middleware:', error);
+      res.status(500).send('Internal Server Error');
     }
-} catch (error) {
-  console.error('Error in isAuthenticated middleware:', error);
-  res.status(500).send('Internal Server Error');
-}
-};
+  },
   
   // Middleware function to check if the user is an admin
-  exports.isAdmin = (req, res, next) => {
+  isAdmin: (req, res, next) => {
     try {
-    if (req.session.user && req.session.user.isAdmin) {
-      // User is an admin
-      next();
-    } else {
-      // User is not an admin
-      res.status(403).send('Forbidden');
+      if (req.session.user && req.session.user.isAdmin) {
+        // User is an admin
+        next();
+      } else {
+        // User is not an admin
+        res.status(403).send('Forbidden');
+      }
+    } catch (error) {
+      console.error('Error in isAdmin middleware:', error);
+      res.status(500).send('Internal Server Error');
     }
-  } catch (error) {
-    console.error('Error in isAdmin middleware:', error);
-    res.status(500).send('Internal Server Error');
-}
+  }
 };
-  
+
+module.exports = authMiddleware;
